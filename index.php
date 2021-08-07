f2392871433a5b8c5f3b46dc6636fd516b4d65c7
shafikcmt <mdshafiqulislam822@gmail.com>
2021-08-08 03:31:14 +0600

    Login/Registration system with PHP OOP, PDO & Bootstrap - Design Index Page

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Login Register System</title>
+	<link rel="stylesheet" type="text/css" href="inc/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="inc/style.css">
+</head>
+
+<body>
+	<div class="container">
+<nav class="navbar navbar-default navbar-default" role="navigation">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+          <a class="navbar-brand" href="#">Login Register System PHP & OOP</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+   
+      <ul class="nav navbar-nav navbar-right">
+      		<li>
+      		  <a href="#" class="dropdown-toggle" data-toggle="dropdown"><b>Profile</b></a>
+      	</li>
+      		<li>
+      		  <a href="#" class="dropdown-toggle" data-toggle="dropdown"><b>Logout</b></a>
+      	</li>
+      	<li>
+      		  <a href="#" class="dropdown-toggle" data-toggle="dropdown"><b>Register</b></a>
+      	</li>
+        <li>
+        	
+        	  <a href="#" class="dropdown-toggle" data-toggle="dropdown"><b>Login</b></a>
+        </li>
+       </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+</div>
+<div class="container">
+	<p>body text</p>
+</div>
+
+
+<div class="container">
+<div class="well well-sm">
+	<h3>Email:mdshafiqulislam822@gmail.com <p class="pull-right">Phone : +917430990829</p></h3>
+	
+</div>
+</div>
+
+<script type="text/javascript" src="inc/jquery.min.js"></script>
+<script type="text/javascript" src="inc/bootstrap.min.js"></script>
+</body>
+</html>
+
+
